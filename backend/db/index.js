d90611dfe1e2ad6af2e106d116127d4ed9698441
Arthur <arthurdmouradian@gmail.com>
2021-06-28 17:19:58 -0400

    Pushed backend

---
+++ backend/db/index.js
@@ -0,0 +1,47 @@
+// Loads app server using express.
+const express = require("express");
+const mysql = require("mysql");
+require("dotenv").config();
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: "",
+  user: "",
+  password: "",
+  database: "",
+});
+
+let DB = {};
+
+// Wrapper for sending SQL queries to sql server
+function myQueryer(stringQuery) {
+  return new Promise((resolve, reject) => {
+    pool.query(stringQuery, (err, results) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(results);
+    });
+  });
+}
+
+
+// All Queries will be created below as callable functions
+
+/* 
+All GET queries
+*/
+
+/* 
+CREATION
+*/
+
+/* 
+UPDATION
+*/
+
+/* 
+Deletion
+*/
+
+module.exports = DB;
---
+++ backend/routes/index.js
@@ -0,0 +1,9 @@
+// Loads app server using express.
+const express = require("express");
+const morgan = require("morgan");
+const router = express.Router();
+const db = require("../db");
+
+// Will set up all routes in here
+
+module.exports = router;
